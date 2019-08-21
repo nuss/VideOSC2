e9c1e941edd8aaf1f436176b5b6c54dd5086b28c
Stefan Nussbaumer <st9fan@gmail.com>
2019-08-21 22:23:55 +0200

    update 'about fragment'
    Signed-off-by: Stefan Nussbaumer <st9fan@gmail.com>

--- app/src/main/java/net/videosc/fragments/settings/VideOSCAboutFragment.java
+++ app/src/main/java/net/videosc/fragments/settings/VideOSCAboutFragment.java
@@ -1,13 +1,17 @@
 package net.videosc.fragments.settings;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import net.videosc.R;
 import net.videosc.fragments.VideOSCBaseFragment;
 import net.videosc.fragments.VideOSCCameraFragment;
+
+import java.util.Calendar;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentManager;
@@ -15,7 +19,6 @@ import androidx.fragment.app.FragmentManager;
 public class VideOSCAboutFragment extends VideOSCBaseFragment {
 	/**
 	 * @param savedInstanceState
-	 * @deprecated
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,6 @@ public class VideOSCAboutFragment extends VideOSCBaseFragment {
 	 * @param inflater
 	 * @param container
 	 * @param savedInstanceState
-	 * @deprecated
 	 */
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,16 +37,25 @@ public class VideOSCAboutFragment extends VideOSCBaseFragment {
 		final VideOSCCameraFragment cameraView = (VideOSCCameraFragment) fragmentManager.findFragmentByTag("CamPreview");
 
 		final View view = inflater.inflate(R.layout.about, container, false);
+		setYear(view);
 
 //		return super.onCreateView(inflater, container, savedInstanceState);
 		return view;
 	}
 
-	/**
-	 * @deprecated
-	 */
 	@Override
 	public void onPause() {
 		super.onPause();
 	}
+
+	private void setYear(View container) {
+		Resources res = getResources();
+		String dateString = res.getString(R.string.videosc_copyright);
+		TextView tv = container.findViewById(R.id.videosc_copyright);
+		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+		String formatted = String.format(dateString, String.valueOf(currentYear));
+		tv.setText(formatted);
+	}
+
 }
