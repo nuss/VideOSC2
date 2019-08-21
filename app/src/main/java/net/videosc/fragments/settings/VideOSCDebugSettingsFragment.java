4c0e88a8eab50e957648fe382cebc21f119b069e
Stefan Nussbaumer <st9fan@gmail.com>
2019-08-21 20:00:34 +0200

    'debug settings' fragment functional
    Signed-off-by: Stefan Nussbaumer <st9fan@gmail.com>

--- app/src/main/java/net/videosc/fragments/settings/VideOSCDebugSettingsFragment.java
+++ app/src/main/java/net/videosc/fragments/settings/VideOSCDebugSettingsFragment.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import net.videosc.R;
+import net.videosc.VideOSCApplication;
+import net.videosc.activities.VideOSCMainActivity;
 import net.videosc.fragments.VideOSCBaseFragment;
 import net.videosc.fragments.VideOSCCameraFragment;
 
@@ -33,8 +37,31 @@ public class VideOSCDebugSettingsFragment extends VideOSCBaseFragment {
 		final FragmentManager fragmentManager = getFragmentManager();
 		assert fragmentManager != null;
 		final VideOSCCameraFragment cameraView = (VideOSCCameraFragment) fragmentManager.findFragmentByTag("CamPreview");
+		final VideOSCMainActivity activity = (VideOSCMainActivity) getActivity();
+		assert activity != null;
+		final VideOSCApplication app = (VideOSCApplication) activity.getApplication();
 
 		final View view = inflater.inflate(R.layout.debug_settings, container, false);
+
+		final Switch hidePixelImageCB = view.findViewById(R.id.hide_pixel_image);
+		final Switch debugPixelOscSendingCB = view.findViewById(R.id.add_packet_drops);
+		hidePixelImageCB.setChecked(app.getPixelImageHidden());
+		debugPixelOscSendingCB.setChecked(VideOSCApplication.getDebugPixelOsc());
+
+		hidePixelImageCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				app.setPixelImageHidden(isChecked);
+			}
+		});
+
+		debugPixelOscSendingCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				VideOSCApplication.setDebugPixelOsc(isChecked);
+			}
+		});
+
 
 //		return super.onCreateView(inflater, container, savedInstanceState);
 		return view;
